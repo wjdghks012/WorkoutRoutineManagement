39403c6ec5a172613497696630a0c88d7503cdba
wjdghks012 <bryan0819@naver.com>
2022-03-10 22:52:44 +0900

    added menu.java

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+
+public class Menu {
+	
+	public static void main(String[] args) {
+		System.out.print(" 1. Monday");
+		System.out.print(" 2. Tuesday");
+		System.out.print(" 3. Wednesday");
+		System.out.print(" 4. Thursday");
+		System.out.print(" 5. Friday");
+		System.out.print(" 6. Saturday");
+		System.out.println(" 7. Rest");
+	}
+
+}
