296a12c59091817f84f7590d069a67471d427363
wjdghks012 <bryan0819@naver.com>
2022-03-20 19:30:03 +0900

    add menu selection

--- src/Menu.java
+++ src/Menu.java
@@ -1,14 +1,42 @@
+import java.util.Scanner;
 
 public class Menu {
 	
 	public static void main(String[] args) {
-		System.out.print(" 1. Monday");
-		System.out.print(" 2. Tuesday");
-		System.out.print(" 3. Wednesday");
-		System.out.print(" 4. Thursday");
-		System.out.print(" 5. Friday");
-		System.out.print(" 6. Saturday");
-		System.out.println(" 7. Rest");
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while (num !=6) {
+		System.out.println(" 1. Add Routine");
+		
+		System.out.println(" 2. Delete Routine");
+	
+		System.out.println(" 3. Edit Routine");
+		
+		System.out.println(" 4. View Routine ");
+	
+		System.out.println(" 5. Show a menu");
+	
+		System.out.println(" 6. Exit");
+		
+		System.out.println(" Select one number between 1-6: ");
+		
+		
+		
+		num = input.nextInt();
+		switch(num) {
+		case 1: 
+			System.out.print("User ID : ");
+			int UserID = input.nextInt();
+			System.out.print("Routine Part : ");
+			String RoutinePart = input.next();
+			break;
+		case 2: 
+		case 3: 
+		case 4: System.out.print("Routine Part : ");
+		String RoutinePart2 = input.next();
+			
+		}
 	}
-
+	}
 }
