6ce8e9348cc0bf603b50913fb4d7df878d47d15e
wjdghks012 <bryan0819@naver.com>
2022-05-15 21:54:48 +0900

    push

---
+++ src/User/UserInput.java
@@ -0,0 +1,30 @@
+package User;
+
+import java.util.Scanner;
+
+import exception.EmailFormatException;
+
+public interface UserInput {
+		public int getId();
+		
+		public void setId(int id);
+			
+		public void setName(String name);
+		
+		public void setEmail(String email) throws EmailFormatException;
+		
+		public void setPhone(String phone);
+		
+		public void printInfo();
+		
+		public void setUserID(Scanner input);
+		
+		public void setUserName(Scanner input);
+		
+		public void setUserEmail(Scanner input) throws EmailFormatException;
+		
+		public void setUserPhone(Scanner input);
+		
+		public void getUserInput(Scanner input);
+
+}
